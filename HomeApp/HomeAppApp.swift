b2e3114073c0dda34bc80a6ba671a14cb88cd0f4
Xinlin Zhou <XZhou@lcs.on.ca>
2024-01-09 14:52:48 -0500

    Initial Commit

---
+++ HomeApp/HomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeAppApp.swift
+//  HomeApp
+//
+//  Created by Fiona ZHOU on 2024-01-09.
+//
+
+import SwiftUI
+
+@main
+struct HomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
