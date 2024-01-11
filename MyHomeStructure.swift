173312f46f81c57ee47b3fce365d25c5375a3028
Xinlin Zhou <XZhou@lcs.on.ca>
2024-01-10 19:42:07 -0500

    Successfully builded up a additional structure to avoid repetition.

---
+++ MyHomeStructure.swift
@@ -0,0 +1,18 @@
+//
+//  MyHomeStructure.swift
+//  HomeApp
+//
+//  Created by Fiona ZHOU on 2024-01-10.
+//
+
+import SwiftUI
+
+struct MyHomeStructure: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyHomeStructure()
+}
